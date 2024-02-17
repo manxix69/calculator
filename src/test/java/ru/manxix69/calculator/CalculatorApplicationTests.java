56b103fe5b754c05afa4ff0946d0e33ad8e69996
manxix69 <manxix69@gmail.com>
2024-02-17 14:00:06 +0500

    Initial commit

---
+++ src/test/java/ru/manxix69/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.manxix69.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
